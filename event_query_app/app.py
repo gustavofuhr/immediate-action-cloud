c93db58b7c99b0264145c5f2d5cd1e0c264d3942
Gustavo Führ <gustavofuhr87@gmail.com>
2025-07-02 18:29:36 -0300

    UPD: made tab with alpr and event search

--- event_query_app/app.py
+++ event_query_app/app.py
@@ -13,65 +13,120 @@ st.title("🎥 Event Record Filter")
 #                     'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe']
 
 
-
-col1, col2, col3, col4 = st.columns(4)
+device_ids = event_query.get_all_device_ids()
+col1, col2, col3 = st.columns([1,1,2])
 with col1:
     start_date = st.date_input("Start date", value=date.today())
 with col2:
     end_date = st.date_input("End date", value=date.today())
 with col3:
-    class_filter = st.multiselect(
-        "Select classes (optional):",
-        options=['person', 'animals', 'dog', 'sheep', 'cow', 'bird', 'vehicles', 'car', 'bicycle', 'motorcycle', 'train', 'truck'], 
-        default=["person"]
-    )
-with col4:
-    force_or = "animals" in class_filter or "vehicles" in class_filter
-    if force_or:
-        st.radio(
-            "Match condition", options=["OR"], index=0,
-            disabled=True, horizontal=True, key="forced_or"
-        )
-        logic_operator = "OR"
-    else:
-        logic_operator = st.radio(
-            "Match condition", options=["OR", "AND"], index=0,
-            disabled=len(class_filter) <= 1, horizontal=True, key="user_logic"
-        )
-    
-
-device_ids = event_query.get_all_device_ids()
-col_device, col_threshold = st.columns([2, 1])
-with col_device:
     selected_devices = st.multiselect(
-        "Devices", device_ids, device_ids[:1],
+        "Devices", device_ids + ["any"], "any",
         format_func=lambda x: x.split("__")[1] if "__" in x else x
     )
-with col_threshold:
-    threshold = st.slider("Threshold", 0.0, 1.0, 0.5, 0.05)
 
-start_dt = datetime.combine(start_date, time.min)
-end_dt = datetime.combine(end_date, time.max)
+tab1, tab2, tab3 = st.tabs(["Filter by object", "License plate", "Specific event"])
+with tab1:
+    col_obj1, col_obj2, col_obj3 = st.columns([2,1,1])
+    with col_obj1:
+        class_filter = st.multiselect(
+            "Select classes (optional):",
+            options=['person', 'animals', 'dog', 'sheep', 'cow', 'bird', 'vehicles', 'car', 'bicycle', 'motorcycle', 'train', 'truck'], 
+            default=["person"]
+        )
+    with col_obj2:
+        force_or = "animals" in class_filter or "vehicles" in class_filter
+        if force_or:
+            st.radio(
+                "Match condition", options=["OR"], index=0,
+                disabled=True, horizontal=True, key="forced_or"
+            )
+            logic_operator = "OR"
+        else:
+            logic_operator = st.radio(
+                "Match condition", options=["OR", "AND"], index=0,
+                disabled=len(class_filter) <= 1, horizontal=True, key="user_logic"
+            )
+    with col_obj3:
+        threshold = st.slider("Threshold", 0.0, 1.0, 0.5, 0.05, key="threshold_object")
+
+    
+    if st.button("🔍 Filter Events"):
+        if not selected_devices:
+            st.warning("Please select at least one device (or 'any').")
+        else:
+            start_dt = datetime.combine(start_date, time.min)
+            end_dt = datetime.combine(end_date, time.max)
+            if "any" in selected_devices:
+                selected_devices = device_ids
+
+            print(f"Querying events from {start_dt} to {end_dt} for devices: {selected_devices}, classes: {class_filter}, threshold: {threshold}, condition: {logic_operator}")
+            results = event_query.query_events(
+                start_date=start_dt,
+                end_date=end_dt,
+                target_classes=class_filter,
+                threshold=threshold,
+                condition=logic_operator,
+                device_ids=selected_devices
+            )
+            st.session_state.filtered_results = results
+            st.session_state.random_batch = random.sample(results, min(12, len(results)))
+with tab2:
+    col_lr1, col_lr2, col_lr3 = st.columns([2,1,1])
+    with col_lr1:
+        search_plate = st.text_input("Search plate (ex: 'ABC1234')", "")
+    with col_lr2:
+        plate_threshold = st.slider("Plate threshold", 0.0, 1.0, 0.5, 0.05, key="plate_threshold")
+    with col_lr3:
+        ocr_threshold = st.slider("OCR threshold", 0.0, 1.0, 0.5, 0.05, key="ocr_threshold", disabled=True)
+
+    if st.button("🔍 Search plate"):
+        if not selected_devices or not search_plate:
+            st.warning("Please select at least one device (or 'any') and enter a plate to search for.")
+        else:
+            start_dt = datetime.combine(start_date, time.min)
+            end_dt = datetime.combine(end_date, time.max)
+            if "any" in selected_devices:
+                selected_devices = device_ids
+
+            print(f"Querying events from {start_dt} to {end_dt} for devices: {selected_devices}, plate: {search_plate}, plate threshold: {plate_threshold}, OCR threshold: {ocr_threshold}")
+            results = event_query.query_events_by_plate(
+                start_date=start_dt,
+                end_date=end_dt,
+                search_plate=search_plate,
+                plate_threshold=plate_threshold,
+                ocr_threshold=ocr_threshold,
+                device_ids=selected_devices
+            )
+            st.session_state.filtered_results = results
+            st.session_state.random_batch = random.sample(results, min(12, len(results)))
+with tab3:
+    col_se1, col_se2 = st.columns(2)
+    with col_se1:
+        selected_devices = st.selectbox("Device ID", device_ids,
+            format_func=lambda x: x#.split("__")[1] if "__" in x else x
+        )
+    with col_se2:
+        search_timestamp = st.text_input("Search timestamp (ex: '2025-07-02T17:21:14.861490+00:00')", "")
+    if st.button("🔍 Get event"):
+        if not search_timestamp:
+            st.warning("Please enter a timestamp to search for.")
+        else:
+            results = event_query.get_event_by_id_and_timestamp(
+                device_id=selected_devices,
+                event_timestamp=search_timestamp
+            )
+            st.session_state.filtered_results = [results]
+            st.session_state.random_batch = random.sample([results], min(12, len([results])))
+
+    
+
 
 if "filtered_results" not in st.session_state:
     st.session_state.filtered_results = []
 if "random_batch" not in st.session_state:
     st.session_state.random_batch = []
 
-if st.button("🔍 Filter Events"):
-    if not selected_devices or not class_filter:
-        st.warning("Please select at least one device and one class.")
-    else:
-        results = event_query.query_events(
-            start_date=start_dt,
-            end_date=end_dt,
-            target_classes=class_filter,
-            threshold=threshold,
-            condition=logic_operator,
-            device_ids=selected_devices
-        )
-        st.session_state.filtered_results = results
-        st.session_state.random_batch = random.sample(results, min(12, len(results)))
 
 if st.session_state.filtered_results:
     st.markdown(f"### Showing {len(st.session_state.random_batch)} of {len(st.session_state.filtered_results)} Results")
@@ -89,7 +144,7 @@ if st.session_state.filtered_results:
             with cols[i]:
                 if item["video_url"]:
                     st.video(item["video_url"])
-                st.caption(f"📅 {item['timestamp']}")
+                st.caption(f"📹 {item.get('device_id', 'Unknown')} • 📅 {item['event_timestamp']}")
                 if "detection_stats" in item:
                     class_scores = [
                         f"{cls} ({item['detection_stats'][cls]['max_confidence']:.2f})"
--- event_query_app/event_query.py
+++ event_query_app/event_query.py
@@ -62,17 +62,19 @@ class EventQuery:
                 filtered_items.append(item)
 
         return filtered_items
-
-
-    def _filter_items_by_classes(self, items: list, target_classes: list[str], condition: str = "OR"):
+    
+    def _filter_items_by_plate_text(self, items: list, search_plate: str, plate_threshold: float = 0.0, ocr_threshold: float = 0.0):
         filtered_items = []
         for item in items:
-            if "seen_classes" in item and item["seen_classes"]:
-                seen_classes = set(item["seen_classes"])
-                if condition == "OR" and seen_classes.intersection(target_classes):
-                    filtered_items.append(item)
-                elif condition == "AND" and seen_classes.issuperset(target_classes):
-                    filtered_items.append(item)
+            plate_stats = item.get("plate_recognition_stats", {})
+            passed_plates = [
+                plate for plate in plate_stats
+                if plate_stats[plate].get("max_confidence", 0) >= plate_threshold
+                and search_plate.lower() in plate.lower()
+            ]
+            if passed_plates:
+                filtered_items.append(item)
+
         return filtered_items
 
     def _get_video_presigned_urls(self, items: list, expires_in=3600):
@@ -86,6 +88,27 @@ class EventQuery:
                 )
                 urls.append(url)
         return urls
+    
+    def _item_to_dict(self, item: dict) -> dict:
+        """
+        Convert a DynamoDB item to a dictionary, handling nested structures.
+        """
+        d = {
+            "device_id": item.get("device_id"),
+            "event_timestamp": item.get("event_timestamp"),
+            "seen_classes": item.get("seen_classes", []),
+            "detection_stats": item.get("detection_stats", {}),
+            "seen_plates": item.get("seen_plates", []),
+            "plate_recognition_stats": item.get("plate_recognition_stats", {}),
+            "video_key": item.get("video_key"),
+        }
+        if "video_key" in item:
+            d["video_url"] = self.s3.generate_presigned_url(
+                "get_object",
+                Params={"Bucket": self.bucket_name, "Key": item["video_key"]},
+                ExpiresIn=3600,
+            )
+        return d
 
     def query_events(
         self,
@@ -99,6 +122,8 @@ class EventQuery:
         if not device_ids:
             raise ValueError("device_ids must be a non-empty list.")
         
+        items = self._fetch_dynamo_items(device_ids, start_date, end_date)
+        
         if "animals" in target_classes:
             target_classes.extend(['dog', 'sheep', 'cow', 'bird', 'horse', 'elephant', 'bear', 'zebra', 'giraffe'])
             target_classes.remove('animals')
@@ -106,27 +131,41 @@ class EventQuery:
             target_classes.extend(['car', 'motorcycle', 'bus', 'bicycle', 'train', 'truck', 'boat', 'airplane'])
             target_classes.remove('vehicles')
 
-        items = self._fetch_dynamo_items(device_ids, start_date, end_date)
-        filtered_items = self._filter_items_by_detection_stats(items, target_classes, threshold, condition)
+        if not target_classes:
+            filtered_items = items
+        else:
+            filtered_items = self._filter_items_by_detection_stats(items, target_classes, threshold, condition)
 
-        results = []
-        for item in filtered_items:
-            entry = {
-                "timestamp": item.get("event_timestamp"),
-                "device_id": item.get("device_id"),
-                "seen_classes": item.get("seen_classes", []),
-                "detection_stats": item.get("detection_stats", {}),
-                "video_url": None
-            }
-            if "video_key" in item:
-                entry["video_url"] = self.s3.generate_presigned_url(
-                    "get_object",
-                    Params={"Bucket": self.bucket_name, "Key": item["video_key"]},
-                    ExpiresIn=3600,
-                )
-            results.append(entry)
-
+        results = [self._item_to_dict(item) for item in filtered_items]
         return results
+    
+    def query_events_by_plate(
+        self,
+        start_date: datetime,
+        end_date: datetime,
+        search_plate: str,
+        plate_threshold: float = 0.0,
+        ocr_threshold: float = 0.0,
+        device_ids: list[str] = [],
+    ) -> list[dict]:
+        if not device_ids:
+            raise ValueError("device_ids must be a non-empty list.")
+
+        items = self._fetch_dynamo_items(device_ids, start_date, end_date)
+        filtered_items = self._filter_items_by_plate_text(items, search_plate, plate_threshold, ocr_threshold)
+
+        results = [self._item_to_dict(item) for item in filtered_items]
+        return results
+
+    def get_event_by_id_and_timestamp(self, device_id: str, event_timestamp: str) -> dict | None:
+        response = self.table.get_item(
+            Key={
+                "device_id": device_id,
+                "event_timestamp": event_timestamp
+            }
+        )
+        item = response.get("Item")
+        return self._item_to_dict(item) if item else None
 
 
     @cached(cache=device_id_cache)
@@ -135,23 +174,5 @@ class EventQuery:
         with open("cameras.json", "r") as f:
             data = json.load(f)
             return sorted(data["cameras"])
-        # device_ids = set()
-        # last_key = None
-
-        # while True:
-        #     kwargs = {
-        #         "ProjectionExpression": "device_id"
-        #     }
-        #     if last_key:
-        #         kwargs["ExclusiveStartKey"] = last_key
-
-        #     response = self.table.scan(**kwargs)
-        #     for item in response.get("Items", []):
-        #         if "device_id" in item:
-        #             device_ids.add(item["device_id"])
-
-        #     last_key = response.get("LastEvaluatedKey")
-        #     if not last_key:
-        #         break
-        # print( sorted(device_ids))
-        # return sorted(device_ids)
+        
+    
